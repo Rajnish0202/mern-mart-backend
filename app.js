14f9d3a7d1b66682c0cbecd5a5a19a109371308f
Rajnish0202 <rajnish.0202kumar@gmail.com>
2023-02-22 11:23:16 +0530

    cors fixed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(fileUpload());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://mern-online.onrender.com'],
+    origin: ['http://localhost:3000', 'https://mernmart.vercel.app'],
     credentials: true,
   })
 );
